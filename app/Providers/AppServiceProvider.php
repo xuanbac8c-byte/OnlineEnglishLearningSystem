97cbaa22e324dd8ca42cb04fed0f38ef7869548f
Diep Nguyen <nguyenquangdiepzed1@gmail.com>
2026-05-10 15:28:53 +0700

    feat: inject Dependency Service.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Interfaces\IUserService;
+use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            IUserService::class,
+            UserService::class
+        );
     }
 
     /**
