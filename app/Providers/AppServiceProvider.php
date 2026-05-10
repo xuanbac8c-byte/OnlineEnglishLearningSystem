5bd547c5c95be5d7bf4d6a789f8e1e2976a0099a
Diep Nguyen <nguyenquangdiepzed1@gmail.com>
2026-05-10 19:34:38 +0700

    chore: fix someone error.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,56 @@
 
 namespace App\Providers;
 
+use App\Services\CertificateService;
+use App\Services\CourseReviewService;
+use App\Services\CourseService;
+use App\Services\EnrollmentService;
+use App\Services\Interfaces\ILanguageService;
 use App\Services\Interfaces\IUserService;
+use App\Services\LanguageService;
+use App\Services\LessonProgressService;
+use App\Services\LessonService;
+use App\Services\PaymentService;
+use App\Services\QuizAnswerService;
+use App\Services\QuizAttemptService;
+use App\Services\QuizOptionService;
+use App\Services\QuizService;
+use App\Services\SectionService;
 use App\Services\UserService;
+use ICertificateService;
+use ICourseReviewService;
+use ICourseService;
+use IEnrollmentService;
+use ILessonProgressService;
+use ILessonService;
 use Illuminate\Support\ServiceProvider;
+use IPaymentService;
+use IQuizAnswerService;
+use IQuizAttemptService;
+use IQuizOptionService;
+use IQuizService;
+use ISectionService;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        $this->app->bind(
-            IUserService::class,
-            UserService::class
-        );
+        $this->app->bind(IUserService::class,          UserService::class);
+        $this->app->bind(ILanguageService::class,      LanguageService::class);
+        $this->app->bind(ILessonService::class,        LessonService::class);
+        $this->app->bind(ILessonProgressService::class, LessonProgressService::class);
+        $this->app->bind(ISectionService::class,       SectionService::class);
+        $this->app->bind(ICourseService::class,        CourseService::class);
+        $this->app->bind(ICourseReviewService::class,  CourseReviewService::class);
+        $this->app->bind(IEnrollmentService::class,    EnrollmentService::class);
+        $this->app->bind(IPaymentService::class,       PaymentService::class);
+        $this->app->bind(ICertificateService::class,   CertificateService::class);
+        $this->app->bind(IQuizService::class,          QuizService::class);
+        $this->app->bind(IQuizOptionService::class,    QuizOptionService::class);
+        $this->app->bind(IQuizAnswerService::class,    QuizAnswerService::class);
+        $this->app->bind(IQuizAttemptService::class,   QuizAttemptService::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
--- app/Services/Interfaces/ILanguageService.php
+++ app/Services/Interfaces/ILanguageService.php
@@ -1,14 +1,18 @@
 <?php
 
-    use App\Models\Language;
-    use Illuminate\Support\Collection;
+namespace App\Services\Interfaces;
 
-    interface ILanguageService {
-        public function getAll(): Collection;
-        public function findById(int $id): Language;
-        public function findByCode(string $code): Language;
-        public function create(array $data): Language;
-        public function update(int $id, array $data): Language;
-        public function delete(int $id): bool;
-    }
+use App\Models\Language;
+use Illuminate\Support\Collection;
+
+interface ILanguageService
+{
+    public function getAll(): Collection;
+    public function findById(int $id): Language;
+    public function findByCode(string $code): Language;
+    public function create(array $data): Language;
+    public function update(int $id, array $data): Language;
+    public function delete(int $id): bool;
+}
+
 ?>
